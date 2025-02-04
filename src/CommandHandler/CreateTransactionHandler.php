5f92c42c9b9c76a7d34eb6d2bbfb77ef8fe44303
Jove Trajkoski <jovetrajkoskidev@gmail.com>
2025-02-05 00:31:23 +0100

    Remove old method implementation

--- src/CommandHandler/CreateTransactionHandler.php
+++ src/CommandHandler/CreateTransactionHandler.php
@@ -91,32 +91,4 @@ class CreateTransactionHandler
 
     return $transaction;
   }
-
-  // public function handle(CreateTransactionDTO $dto): Transaction
-  // {
-  //   $ledger = $this->em->getRepository(Ledger::class)->find($dto->ledgerId);
-  //   if (!$ledger) {
-  //     throw new \Exception('Ledger not found');
-  //   }
-
-  //   $transaction = new Transaction();
-  //   $transaction->setLedger($ledger);
-  //   $transaction->setTransactionType($dto->type);
-  //   $transaction->setAmount($dto->amount);
-  //   $transaction->setCurrency($dto->currency);
-  //   $transaction->setCreatedAt(new \DateTime());
-
-  //   // Update Ledger balance accordingly
-  //   if ($dto->type === 'credit') {
-  //     $ledger->setBalance($ledger->getBalance() + $dto->amount);
-  //   } else {
-  //     $ledger->setBalance($ledger->getBalance() - $dto->amount);
-  //   }
-
-  //   $this->em->persist($transaction);
-  //   $this->em->persist($ledger);
-  //   $this->em->flush();
-
-  //   return $transaction;
-  // }
 }
